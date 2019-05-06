1f3e4aaba79aa69e95051b90c75b1afea526b9a1
Renan Gustavo <renanzx@live.com>
2019-05-06 15:23:16 -0300

    Ex até Sessão 3 Aula 15

---
+++ CursoDevWebModerno/exerceicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4 //Maneira nova de declarar variáveis, mais recomendado
+
+var a = 30 //Pode declarar duas vezes no mesmo código mas não deve
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+//const c = 50
+
+console.log(c)
