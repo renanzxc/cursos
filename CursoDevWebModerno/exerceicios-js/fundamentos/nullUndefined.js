c5298e9e308d3583e5cd775f59be458c670e053c
Renan Gustavo <renanzx@live.com>
2019-05-08 14:39:22 -0300

    Curso

---
+++ CursoDevWebModerno/exerceicios-js/fundamentos/nullUndefined.js
@@ -0,0 +1,22 @@
+let valor // Declarado mas não inicializado
+console.log(valor)
+
+valor = null // Ausência de valor
+console.log(valor)
+// console.log(valor.toString()) //Erro
+
+const produto = {}
+console.log(produto.preco) // undefined
+console.log(produto)  
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined // Evitar atribuir undefined
+console.log(!!produto.preco)
+// delete produto.preco // Deletar atributo
+console.log(produto)
+
+produto.preco = null // Sem preço
+console.log(!!produto.preco)
+console.log(produto)
